e6933dcb6c5bdd7f6fe1beef2463a69838b7331f
LTLiem <lthanhliem38@gmail.com>
2014-11-17 07:31:23 +0700

    Update

---
+++ QLThuVien/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QLThuVien.Models;
+
+namespace QLThuVien.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(nhanvien nv) //User
+        {
+            int MaLoaiNhanVien = 0;
+            string TenLoaiNhanVien = "";
+            string redirectPage = "";
+
+            if (ModelState.IsValid)
+            {
+                using (ThuVienEntities TE = new ThuVienEntities())
+                {
+                    var v = TE.nhanvien.Where(a => (a.tenDangNhap.Equals(nv.tenDangNhap)
+                                && a.matKhau.Equals(nv.matKhau))).FirstOrDefault();  
+   
+                    if (v != null)
+                    {
+                        Session["LogedUserID"] = v.maNhanVien.ToString();
+                        Session["Username"] = v.tenDangNhap.ToString();
+                        Session["Password "] = v.matKhau.ToString();
+
+                        MaLoaiNhanVien = Convert.ToInt32(v.LoaiNhanVien);
+                        TenLoaiNhanVien = TE.loainhanvien.Where(i => i.id == MaLoaiNhanVien).FirstOrDefault().TenLoai;
+
+                        switch (TenLoaiNhanVien)
+                        {
+                            case "Bien muc" :
+                                redirectPage = "BienMucHome";
+                                break;
+                            case "Luu hanh":
+                                redirectPage = "LuuHanhHome";
+                                break;
+                            case "QLDG":
+                                redirectPage = "QLDocGiaHome";
+                                break;
+                        }
+                        return RedirectToAction(redirectPage);
+                    }
+                    ViewBag.Message = "Mật khẩu hoặc tên đăng nhập không hợp lệ.";
+                    return View(nv);
+                }
+            }
+            return View(nv);
+        }
+
+        public ActionResult LuuHanhHome()
+        {
+            if(@Session["Username"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+    }
+}
---
+++ QLThuVien/Controllers/NVLuuHanhController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLThuVien.Controllers
+{
+    public class NVLuuHanhController : Controller
+    {
+        //
+        // GET: /NVLuuHanh/
+
+        public ActionResult NVLuuHanhHome()
+        {
+            return View();
+        }
+
+    }
+}
